2a15392d2d2ef85ef4e70131af86b7523fd3fa43
Hugo Amiard <hugo.amiard@laposte.net>
2020-05-03 02:19:30 +0200

    Handle Vulkan V-flip properly

--- src/renderer_vk.cpp
+++ src/renderer_vk.cpp
@@ -732,7 +732,7 @@ VK_IMPORT_DEVICE
 			ni.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_OBJECT_NAME_INFO_EXT;
 			ni.pNext = NULL;
 			ni.objectType   = getType<Ty>();
-			ni.objectHandle = uint64_t(_object.vk);
+			ni.objectHandle = uint64_t(_object);
 			ni.pObjectName  = temp;
 
 			VK_CHECK(vkSetDebugUtilsObjectNameEXT(_device, &ni) );
@@ -1609,11 +1609,12 @@ VK_IMPORT_INSTANCE
 			}
 
 			{
-				uint32_t numEnabledExtensions = 1;
+				uint32_t numEnabledExtensions = 2;
 
-				const char* enabledExtension[Extension::Count + 1] =
+				const char* enabledExtension[Extension::Count + 2] =
 				{
 					VK_KHR_SWAPCHAIN_EXTENSION_NAME,
+					VK_KHR_MAINTENANCE1_EXTENSION_NAME
 				};
 
 				for (uint32_t ii = 0; ii < Extension::Count; ++ii)
@@ -2598,9 +2599,9 @@ VK_IMPORT_DEVICE
 
 			VkViewport vp;
 			vp.x        = 0;
-			vp.y        = 0;
+			vp.y        = (float)height;
 			vp.width    = (float)width;
-			vp.height   = (float)height;
+			vp.height   = -(float)height;
 			vp.minDepth = 0.0f;
 			vp.maxDepth = 1.0f;
 			vkCmdSetViewport(m_commandBuffer, 0, 1, &vp);
@@ -5899,9 +5900,9 @@ VK_DESTROY
 
 						VkViewport vp;
 						vp.x        = rect.m_x;
-						vp.y        = rect.m_y;
+						vp.y        = rect.m_y + rect.m_height;
 						vp.width    = rect.m_width;
-						vp.height   = rect.m_height;
+						vp.height   = -(float)rect.m_height;
 						vp.minDepth = 0.0f;
 						vp.maxDepth = 1.0f;
 						vkCmdSetViewport(m_commandBuffer, 0, 1, &vp);
